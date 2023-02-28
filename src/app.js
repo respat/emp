3d594e8108a7002dfa67828448afe214624a95b2
respat <respat>
2023-02-28 19:03:09 +0100

    dolgozók hozzáadása

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
+const dolgozoTorzs = document.querySelector("#dolgozoTorzs");
+const nameInput = document.querySelector('#nameInput');
+const cityInput = document.querySelector('#cityInput');
+const salaryInput = document.querySelector('#salaryInput');
+const addButtonSave = document.querySelector('#addButtonSave');
+
+
+
 const host = 'http://localhost:3000/';
 const endpoint = 'employees';
 const url = host + endpoint;
 
-fetch(url)
-.then( (response) => response.json() )
-.then( (result) => {
-    console.log(result);
-    loadEmployees(result);
-})
-.catch( (err) => console.log(err));
+function getEmloyees() {
+    fetch(url)
+    .then( (response) => response.json() )
+    .then( (result) => {
+        console.log(result);
+        loadEmployees(result);
+    })
+    .catch( (err) => console.log(err));
+}
 
+getEmloyees();
 
-const dolgozoTorzs = document.querySelector("#dolgozoTorzs");
 
 function loadEmployees(dolgozoLista) {
     dolgozoLista.forEach((dolgozo) => {
@@ -30,4 +40,35 @@ function loadEmployees(dolgozoLista) {
         tr.append(tdCity);
         tr.append(tdSalary);
     });
+}
+
+addButtonSave.addEventListener('click', () => {
+    addEmployee();
+    clearFields();
+});
+
+function clearFields() {
+    nameInput.value = "";
+    cityInput.value = "";
+    salaryInput.value = "";
+}
+
+function addEmployee() {
+    let name = nameInput.value;
+    let city = cityInput.value;
+    let salary = salaryInput.value;
+    fetch(url,{
+        method: 'POST',
+        body: JSON.stringify({
+            "name": name,
+            "city": city,
+            "salary": salary
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
 }
